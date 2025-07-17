d26ecbbc361025e9efe67cdd8678ebacc9d2194b
andybabu22 <andybabau8@gmail.com>
2025-07-17 22:05:15 +0530

    Update check-indexing-status.js

--- pages/api/check-indexing-status.js
+++ pages/api/check-indexing-status.js
@@ -13,8 +13,7 @@ export default async function handler(req, res) {
     });
 
     const statusData = await statusRes.json();
-    console.log('Status response:', statusData);
-
+    console.log("Status API response:", statusData);
 
     res.status(200).json({
       status: statusData.result ? statusData.result[0] : null
